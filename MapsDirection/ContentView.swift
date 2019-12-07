e1e930a4806400b4270312db388b3ad5f6d95706
stepanet <stepanet@gmail.com>
2019-12-08 01:41:56 +0300

    Initial Commit

---
+++ MapsDirection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapsDirection
+//
+//  Created by Jack Sp@rroW on 08.12.2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
