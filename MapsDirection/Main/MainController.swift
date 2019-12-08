2d9a13e4ffca65567a1f28f9dd54f920a3df1781
stepanet <stepanet@gmail.com>
2019-12-09 00:57:50 +0300

    добавил аннотации

--- MapsDirection/Main/MainController.swift
+++ MapsDirection/Main/MainController.swift
@@ -10,6 +10,17 @@ import UIKit
 import MapKit
 import LBTATools
 
+extension MainController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
+        
+        annotationView.canShowCallout = true
+        return annotationView
+    }
+}
+
+
 class MainController: UIViewController {
     
     let mapView = MKMapView()
@@ -17,24 +28,45 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        mapView.delegate = self
+        
         view.addSubview(mapView)
         mapView.fillSuperview()
-        
-        setupRegionForMap()
         mapView.mapType = .hybrid
+        setupRegionForMap()
+        
+        setupAnnotationsForMap()
+        
+    }
+    
+    
+    fileprivate func setupAnnotationsForMap() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
+        annotation.title = "Это Москва"
+        annotation.subtitle = "RU"
+        mapView.addAnnotation(annotation)
+        
+        
+        let sp = MKPointAnnotation()
+        sp.coordinate = CLLocationCoordinate2D(latitude: 59.937500, longitude: 30.308611)
+        sp.title = "St Petersburg"
+        sp.subtitle = "RU"
+        mapView.addAnnotation(sp)
+        
+        
+        mapView.showAnnotations(self.mapView.annotations, animated: true)
+        
         
     }
     
     fileprivate func setupRegionForMap() {
         let centerCoordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region =  MKCoordinateRegion(center: centerCoordinate, span: span)
         
         mapView.setRegion(region, animated: true)
-        
     }
-    
 }
 
 
