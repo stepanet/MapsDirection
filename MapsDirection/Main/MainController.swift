092a64d7c6579174ce748a17f08691125ea0c73e
stepanet <stepanet@gmail.com>
2019-12-10 02:01:47 +0300

    добавил адрес по точкам

--- MapsDirection/Main/MainController.swift
+++ MapsDirection/Main/MainController.swift
@@ -41,7 +41,7 @@ class MainController: UIViewController {
     
     fileprivate func performLocalSearch()  {
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = "кафе"
+        request.naturalLanguageQuery = "Apple"
         request.region = mapView.region
         
         let localSearch = MKLocalSearch(request: request)
@@ -53,11 +53,38 @@ class MainController: UIViewController {
             
             //sucess
             resp?.mapItems.forEach({ (mapItem) in
-                print(mapItem.placemark.subThoroughfare ?? "")
+                //print(mapItem.placemark.subThoroughfare ?? "")
+                
+                let placemark = mapItem.placemark
+                var addressString = ""
+                
+                if placemark.subThoroughfare != nil {
+                    addressString += placemark.subThoroughfare! + " "
+                }
+                if placemark.thoroughfare != nil {
+                    addressString += placemark.thoroughfare! + " "
+                }
+                if placemark.postalCode != nil {
+                    addressString += placemark.postalCode! + " "
+                }
+                if placemark.locality != nil {
+                    addressString += placemark.locality! + " "
+                }
+                if placemark.administrativeArea != nil {
+                    addressString += placemark.administrativeArea! + " "
+                }
+                if placemark.country != nil {
+                    addressString += placemark.country! + " "
+                }
+                
+                print(addressString)
+                
+                
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = mapItem.placemark.coordinate
                 annotation.title = mapItem.name
                 self.mapView.addAnnotation(annotation)
+                
             })
             
             self.mapView.showAnnotations(self.mapView.annotations, animated: true)
@@ -79,11 +106,9 @@ class MainController: UIViewController {
         sp.title = "St Petersburg"
         sp.subtitle = "RU"
         mapView.addAnnotation(sp)
-        
-        
+                
         mapView.showAnnotations(self.mapView.annotations, animated: true)
-        
-        
+ 
     }
     
     fileprivate func setupRegionForMap() {
