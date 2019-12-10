c7382a5ec676745bf960cc52598466f48e904051
jack sp@rrow <stepanet@gmail.com>
2019-12-11 02:26:01 +0300

    после замены кода на код брайна

--- MapsDirection/Main/MainController.swift
+++ MapsDirection/Main/MainController.swift
@@ -1,9 +1,9 @@
 //
 //  MainController.swift
-//  MapsDirection
+//  MapsDirectionsGooglePlaces_LBTA
 //
-//  Created by Jack Sp@rroW on 08.12.2019.
-//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//  Created by Brian Voong on 11/3/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
 //
 
 import UIKit
@@ -13,13 +13,14 @@ import LBTATools
 extension MainController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
         
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
         annotationView.canShowCallout = true
+//        annotationView.image = #imageLiteral(resourceName: "tourist")
         return annotationView
     }
+    
 }
-
 
 class MainController: UIViewController {
     
@@ -29,52 +30,45 @@ class MainController: UIViewController {
         super.viewDidLoad()
         
         mapView.delegate = self
-        
         view.addSubview(mapView)
         mapView.fillSuperview()
-        //mapView.mapType = .hybrid
         
         setupRegionForMap()
         
-        //setupAnnotationsForMap()
-        //performLocalSearch()
+//        setupAnnotationsForMap()
+//        performLocalSearch()
         setupSearchUI()
     }
     
-    let searchTextField = UITextField(placeholder: "Поиск")
+    let searchTextField = UITextField(placeholder: "Search query")
     
-    
-
     fileprivate func setupSearchUI() {
-
         let whiteContainer = UIView(backgroundColor: .white)
         view.addSubview(whiteContainer)
-        whiteContainer.layer.cornerRadius = 15
-        whiteContainer.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor,padding: .init(top: 0, left: 16, bottom: 0, right: 16), size: .init(width: 0, height: 50))
+        whiteContainer.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
         
         whiteContainer.stack(searchTextField).withMargins(.allSides(16))
         
-        //listen for text changes OLD SCOOL
+        // listen for text changes and then perform new search
+        // OLD SCHOOL
         searchTextField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
         
-        //NEW SCOOL
         
+        // NEW SCHOOL Search Throttling
+        // search on the last keystroke of text changes and basically wait 500 milliseconds
 //        NotificationCenter.default
-//        .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
-//        .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
-//        .sink { (_) in
-//                print(1231230)
+//            .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
+//            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+//            .sink { (_) in
 //                self.performLocalSearch()
 //        }
-        
     }
     
     @objc fileprivate func handleSearchChanges() {
         performLocalSearch()
     }
     
-    
-    fileprivate func performLocalSearch()  {
+    fileprivate func performLocalSearch() {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = searchTextField.text
         request.region = mapView.region
@@ -82,51 +76,46 @@ class MainController: UIViewController {
         let localSearch = MKLocalSearch(request: request)
         localSearch.start { (resp, err) in
             if let err = err {
-                print("Failed local search", err)
+                print("Failed local search:", err)
                 return
             }
             
-            //sucess
+            // Success
+            // remove old annotations
             self.mapView.removeAnnotations(self.mapView.annotations)
+            
             resp?.mapItems.forEach({ (mapItem) in
-
-                //let placemark = mapItem.placemark
+                print(mapItem.address())
+                
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = mapItem.placemark.coordinate
                 annotation.title = mapItem.name
                 self.mapView.addAnnotation(annotation)
-                
             })
-            
             self.mapView.showAnnotations(self.mapView.annotations, animated: true)
         }
-        
     }
-    
     
     fileprivate func setupAnnotationsForMap() {
         let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-        annotation.title = "Это Москва"
-        annotation.subtitle = "RU"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        annotation.title = "San Francisco"
+        annotation.subtitle = "CA"
         mapView.addAnnotation(annotation)
         
+        let appleCampusAnnotation = MKPointAnnotation()
+        appleCampusAnnotation.coordinate = .init(latitude: 37.3326, longitude: -122.030024)
+        appleCampusAnnotation.title = "Apple Campus"
+        appleCampusAnnotation.subtitle = "Cupertino, CA"
+        mapView.addAnnotation(appleCampusAnnotation)
         
-//        let sp = MKPointAnnotation()
-//        sp.coordinate = CLLocationCoordinate2D(latitude: 59.937500, longitude: 30.308611)
-//        sp.title = "St Petersburg"
-//        sp.subtitle = "RU"
-//        mapView.addAnnotation(sp)
-                
         mapView.showAnnotations(self.mapView.annotations, animated: true)
- 
     }
     
     fileprivate func setupRegionForMap() {
-        let centerCoordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
-        let region =  MKCoordinateRegion(center: centerCoordinate, span: span)
-        
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
         mapView.setRegion(region, animated: true)
     }
 }
@@ -134,32 +123,29 @@ class MainController: UIViewController {
 extension MKMapItem {
     func address() -> String {
         var addressString = ""
-        
         if placemark.subThoroughfare != nil {
-            addressString += placemark.subThoroughfare! + " "
+            addressString = placemark.subThoroughfare! + " "
         }
         if placemark.thoroughfare != nil {
-            addressString += placemark.thoroughfare! + " "
+            addressString += placemark.thoroughfare! + ", "
         }
         if placemark.postalCode != nil {
             addressString += placemark.postalCode! + " "
         }
         if placemark.locality != nil {
-            addressString += placemark.locality! + " "
+            addressString += placemark.locality! + ", "
         }
         if placemark.administrativeArea != nil {
             addressString += placemark.administrativeArea! + " "
         }
         if placemark.country != nil {
-            addressString += placemark.country! + " "
+            addressString += placemark.country!
         }
-        
         return addressString
     }
 }
 
-//SwiftUI preview
-
+// SwiftUI Preview
 import SwiftUI
 
 struct MainPreview: PreviewProvider {
@@ -178,7 +164,5 @@ struct MainPreview: PreviewProvider {
         }
         
         typealias UIViewControllerType = MainController
-        
     }
-    
 }
